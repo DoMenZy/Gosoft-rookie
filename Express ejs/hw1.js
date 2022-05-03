ff94a82b0ae8ae1a25fb883d58fce72b6a58181b
DoMenZy <hokagenaruto112233@gmail.com>
2022-05-04 00:39:08 +0700

    express

--- Express ejs/hw1.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000;
-
-app.post('input', (req, res) => {
-    res.send('hello world')
-  })
-  app.listen(8080, 'localhost')
--- HTTP/hw1.js
+++ HTTP/hw1.js
@@ -1,31 +1,84 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/dodo', (req, res) => {
-    console.log(req.query);
-    res.status(400).send("hi");
+let emp = [];
+
+app.get('/getEmp', (req, res) => {
+    res.status(200).json({
+        data: emp
+    });
 })
 
-app.post('/dodo1', (req, res) => {
-    console.log(req.body);
-    console.log(req.body.n1 + req.body.n2);
-    let sum = req.body.n1 + req.body.n2
-    res.send('' + sum);
+app.post('/newEmp', (req, res) => {
+
+    if (!req.body.fname ||
+        !req.body.lname ||
+        !req.body.id ||
+        !req.body.pos ||
+        !req.body.tel ||
+        !req.body.email
+
+    ) { return res.status(400).json({ ms: "Erorr" }) }
+
+    for (let i = 0; i < emp.length; i++) {
+        if (emp[i].id == req.body.id || emp[i].tel == req.body.tel || emp[i].email == req.body.email) {
+            return res.status(400).json({ ms: "Erorr = ข้อมุลซ้ำ" })
+        }
+    }
+
+    const newData = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        id: req.body.id,
+        pos: req.body.pos,
+        tel: req.body.tel,
+        email: req.body.email
+    };
+
+    emp.push(newData);
+    res.status(200).json({ ms: "Success" });
+
 })
 
-app.put('/dodo2', (req, res) => {
-    console.log(req.body);
-    console.log(req.body.n1 + req.body.n2);
-    let sum = req.body.n1 + req.body.n2
-    res.send('' + sum);
+app.put('/editEmp', (req, res) => {
+
+    if (
+        !req.body.id ||
+        (
+            !req.body.pos &&
+            !req.body.tel &&
+            !req.body.email
+        )
+    ) { return res.status(400).json({ ms: "Erorr" }) }
+
+    for (let i = 0; i < emp.length; i++) {
+        if (emp[i].id == req.body.id) {
+
+            if (req.body.pos) emp[i].pos = req.body.pos
+            if (req.body.tel) emp[i].tel = req.body.tel
+            if (req.body.email) emp[i].email = req.body.email
+
+            return res.status(200).json({ ms: "Success" })
+        }
+    }
+
+    return res.status(400).json({ ms: "Erorr ไม่เจอคนที่จะเเก้ไข" });
 })
 
-app.delete('/dodo3', (req, res) => {
-    console.log(req.body);
-    console.log(req.body.n1 + req.body.n2);
-    let sum = req.body.n1 + req.body.n2
-    res.send('' + sum);
+app.delete('/deleteEmp', (req, res) => {
+    if (!req.body.id) {
+        return res.status(400).json({ ms: "Erorr" })
+    }
+    for (let i = 0; i < emp.length; i++) {
+        if (emp[i].id == req.body.id) {
+            emp.splice(i, 1);
+            return res.status(200).json({ ms: "Success" })
+        }
+    }
+
+    return res.status(400).json({ ms: "Erorr ไม่เจอคนที่จะลบ" });
 })
 
 app.listen(3000, () => {
